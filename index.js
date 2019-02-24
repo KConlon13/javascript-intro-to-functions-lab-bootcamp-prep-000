fd026f9b6bdcead21a3a7862168be18fa78a6890
Kyle Conlon <kyleconlon13@gmail.com>
2019-02-24 19:04:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,21 +22,13 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase()===lowercase){
     return "I can\'t hear you!"
   }
-  
   else if (string.toUpperCase()===uppercase){
     return "YES INDEED!"
   }
-  
-  else if (string.()===mixedcase){
+  else{
     return "I love you, too."
   }
   
-  
-  
-  
-  //else if (string.toUpperCase()===mixedcase)
-  //else if (string.toLowerCase()===mixedcase){
-    //return "I love you, too."}
 }
 
 
